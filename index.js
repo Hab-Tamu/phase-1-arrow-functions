2576940aa8b7dd2b79019b3834829b8ceab511f4
Habtamu Debele <habtlig2010@gmail.com>
2022-03-24 19:04:45 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+//Functions using arrow 
+
+function divide(){
+    let num1 = 2000
+    let num2 = 100
+    return num1/num2
+}
+divide()
+// console.log(divide())
+
+const square = x  => x*x
+square()
+// console.log(square(2))
+
+const add = (num1, num2) => num1 + num2
+add()
+// console.log(add(4,4))
+
